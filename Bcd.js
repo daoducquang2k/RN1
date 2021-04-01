a44dd6109c1f454e9227923f8a568f954a8a0972
Dao Quang <kakaquang2000pt@gmail.com>
2021-04-01 20:36:39 +0700

    dc

--- Bcd.js
+++ Bcd.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function Bcd() {
     return (
         <View>
-            <Text></Text>
+            <Text>dfsdfsd</Text>
         </View>
     )
 }
