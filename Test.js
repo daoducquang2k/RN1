be5a0872e68232edefc1ae16d618bb8eb4e94c73
Dao Quang <kakaquang2000pt@gmail.com>
2021-04-01 20:25:48 +0700

    login

---
+++ Test.js
@@ -0,0 +1,10 @@
+//rnf
+import React from 'react'
+import { View, Text } from 'react-native'
+export default function Test() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
